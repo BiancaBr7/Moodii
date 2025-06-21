0ed44cd20435daea0c27afcd2cda9d0abf752d3c
Bianca Bai <biancabaixiuqi@outlook.com>
2025-06-21 00:01:37 -0400

    setting up backend

---
+++ backend/moodii/src/main/java/com/moodii/MoodiiApplication.java
@@ -0,0 +1,13 @@
+package com.moodii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodiiApplication.class, args);
+	}
+
+}
---
+++ backend/moodii/src/test/java/com/moodii/MoodiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
